e88f46f5f62f37f3d8de59cc68097c86152d75d9
Alexander Koval <alexander.koval@ix.co>
2021-08-24 21:48:54 +0300

    Add spinner

--- src/ProcessingLog.js
+++ src/ProcessingLog.js
@@ -7,10 +7,10 @@ function ProcessingLog({ events }) {
             <div class="container px-5 mx-auto flex flex-wrap">
                 <div class="flex flex-wrap w-full mx-auto justify-center">
                     <div class="lg:w-3/5 md:w-1/2 md:pr-10 md:py-6">
-                        { events.map(e =>
+                        { events.map((e, num) =>
                             e.last ?
                             <ProcessingFinishStep /> :
-                            <ProcessingStep title={e.step} image={e.picture} description={new Date(e.time*1e3).toLocaleTimeString()}/>
+                            <ProcessingStep title={e.step} image={e.picture} loader={num === events.length-1} description={new Date(e.time*1e3).toLocaleTimeString()}/>
                         )}
                     </div>
                 </div>
--- src/processingStep.js
+++ src/processingStep.js
@@ -1,4 +1,4 @@
-function ProcessingStep({ title, description, image }) {
+function ProcessingStep({ title, description, image, loader=false }) {
     return (
         <div class="flex relative pb-12">
           <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
@@ -10,7 +10,14 @@ function ProcessingStep({ title, description, image }) {
             </svg>
           </div>
           <div class="flex-grow pl-4">
-            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">{ title }</h2>
+            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 inline-flex tracking-wider">
+                { title }
+                { loader &&
+                    <div class=" flex justify-center items-center mx-5">
+                        <div class="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-purple-500"></div>
+                    </div>
+                }
+            </h2>
             { image && <img src={`data:image/jpg;base64,${image}`} /> }
             <p class="leading-relaxed">{ description }</p>
           </div>

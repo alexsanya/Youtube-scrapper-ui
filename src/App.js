4393b7faa22ecf3ecd67f353b4a11f923a8519f7
Alexander Koval <alexander.koval@ix.co>
2021-08-25 11:00:52 +0300

    Add video preview and hide form during processing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,38 @@ import { useState } from 'react';
 import MainForm from './MainForm';
 import ProcessingLog from './ProcessingLog';
 import ProcessingService from './ProcessingService';
+import VideoInfo from './VideoInfo';
 
 function App() {
   const [events, setEvents] = useState([]);
+  const [displayForm, setDisplayForm] = useState(true);
+  const [showTryAgain, setShowTryAgain] = useState(false);
+  const [videoInfo, setVideoInfo] = useState(false);
   const onEvent = e => {
-      setEvents(events => [...events, e]);
+    setEvents(events => [...events, e]);
+    if (e.last) {
+        setShowTryAgain(true);
+    }
   }
-  const startProcessing = videoUrl => { ProcessingService.process(videoUrl, onEvent) }
+  const tryAnother = () => {
+      setShowTryAgain(false);
+      setEvents([]);
+      setDisplayForm(true);
+      setVideoInfo(false);
+  }
+  const startProcessing = async videoUrl => {
+    const videoInfo = await ProcessingService.getVideoInfo(videoUrl);
+    setVideoInfo(videoInfo);
+    ProcessingService.process(videoUrl, onEvent);
+    setDisplayForm(false);
+  }
 
   return (
       <>
-        <MainForm onVideoSelected={startProcessing} />
-        <ProcessingLog events={events}>
-        </ProcessingLog>
+        { displayForm && <MainForm onVideoSelected={startProcessing} /> }
+        { videoInfo && <VideoInfo data={videoInfo} /> }
+        <ProcessingLog events={events} />
+        {showTryAgain && <button onClick={tryAnother} class="group relative w-1/2 flex justify-center py-2 px-4 mx-auto my-10 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Try with another video</button>}
       </>
   );
 }
--- src/ProcessingService.js
+++ src/ProcessingService.js
@@ -1,4 +1,6 @@
+import axios from 'axios';
 const SERVER_URL = 'ws://localhost:8000/process';
+const VIDEO_INFO_URL = 'http://localhost:8000/video-info';
 
 const ProcessingAPI = {
     process: (videoUrl, onMessage) => {
@@ -6,7 +8,8 @@ const ProcessingAPI = {
         socket.addEventListener('message', event => {
             onMessage(JSON.parse(event.data));
         });
-    }
+    },
+    getVideoInfo: async videoUrl => axios.get(`${VIDEO_INFO_URL}?video_url=${videoUrl}`).then(resp => resp.data)
 }
 
 export default ProcessingAPI;
